b99fc1317c6a840164c99bc61f1aaedacf75ea27
Jens Langhammer <jens.langhammer@beryju.org>
2020-10-31 20:35:53 +0100

    web: make logo go up not to root

--- web/src/gp-header.js
+++ web/src/gp-header.js
@@ -32,8 +32,10 @@ class GpApp extends LitElement {
     }
 
     logoClick() {
+        const url = new URL(this.path + "/", window.location);
+        const destUrl = new URL("..", url).pathname.slice(0, -1)
         this.dispatchEvent(new CustomEvent('navigate', {
-            detail: '/', composed: true, bubbles: true,
+            detail: destUrl, composed: true, bubbles: true,
         }));
     }
 
