18e180270d0c24e35d31a9e001b2ca5ddd81e144
Jens Langhammer <jens.langhammer@beryju.org>
2020-10-31 18:48:47 +0100

    web: redesign login form

--- web/src/gp-gate.js
+++ web/src/gp-gate.js
@@ -10,6 +10,41 @@ class GpGate extends LitElement {
                 display: block;
                 min-height: 100vh;
             }
+            div {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 60vh;
+            }
+            form {
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                width: 30rem;
+                margin: auto;
+                padding: 3rem;
+                background-color: var(--color-primary-background-dark);
+                box-shadow: 0px 2px 3px 0px #0008;
+            }
+            form > input, form > button {
+                margin: 1rem 0;
+                padding: 0 1rem;
+                line-height: 3rem;
+                background-color: var(--color-primary-background-light);
+                outline: none;
+                border: 0;
+                color: var(--color-primary-text);
+                transition: 0.25s border-bottom;
+                transition: 0.25s background-color;
+                border-bottom: 1px solid rgba(0,0,0,0);
+            }
+            input:focus, textarea:focus, select:focus{
+                outline: none;
+                border-bottom: 1px solid var(--color-primary);
+            }
+            button:hover, input[type=submit]:hover {
+                background-color: var(--color-primary);
+            }
         `;
     }
 
@@ -31,11 +66,13 @@ class GpGate extends LitElement {
         } else {
             return html`
                 <gp-header></gp-header>
-                <form @submit=${this.submitLogin}>
-                    <input type="text" placeholder="username" name="username" />
-                    <input type="password" placeholder="password" name="password" />
-                    <input type="submit" value="submit" />
-                </form>
+                <div>
+                    <form @submit=${this.submitLogin}>
+                        <input type="text" placeholder="username" name="username" required/>
+                        <input type="password" placeholder="password" name="password" required/>
+                        <input type="submit" value="login" />
+                    </form>
+                </div>
             `;
         }
     }
