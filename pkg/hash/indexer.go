69ef63853eaec88d4b59689e10be8109f0b18274
Jens Langhammer <jens.langhammer@beryju.org>
2021-12-26 23:29:12 +0100

    add files with just their name

--- pkg/hash/indexer.go
+++ pkg/hash/indexer.go
@@ -2,7 +2,9 @@ package hash
 
 import (
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 	"sync"
 
 	"beryju.org/imagik/pkg/config"
@@ -57,26 +59,31 @@ func (hm *HashMap) UpdateSingle(path string) *FileHash {
 	return hm.hashFile(path, stat, err)
 }
 
-func (hm *HashMap) hashFile(path string, info os.FileInfo, err error) *FileHash {
+func (hm *HashMap) hashFile(p string, info os.FileInfo, err error) *FileHash {
+	if err != nil {
+		hm.logger.Warning(err)
+	}
+
 	if info.IsDir() {
 		return nil
 	}
 
-	hashes, err := HashesForFile(path)
+	hashes, err := HashesForFile(p)
 	if err != nil {
 		// Don't return the error to not stop the walking
 		hm.logger.Warning(err)
 	}
+	base := path.Base(p)
+	ext := path.Ext(base)
+	filename := strings.Replace(base, ext, "", 1)
 
 	hm.writeM.Lock()
-	hm.hashMap.Set(hashes.MD5, path)
-	hm.hashMap.Set(hashes.SHA128, path)
-	hm.hashMap.Set(hashes.SHA256, path)
-	hm.hashMap.Set(hashes.SHA512, path)
-	hm.hashMap.Set(hashes.SHA512Short, path)
-	hm.writeM.Unlock()
-	if err != nil {
-		hm.logger.Warning(err)
-	}
+	defer hm.writeM.Unlock()
+	hm.hashMap.Set(filename, p)
+	hm.hashMap.Set(hashes.MD5, p)
+	hm.hashMap.Set(hashes.SHA128, p)
+	hm.hashMap.Set(hashes.SHA256, p)
+	hm.hashMap.Set(hashes.SHA512, p)
+	hm.hashMap.Set(hashes.SHA512Short, p)
 	return hashes
 }
