d280d95bc95f48a06103a7852934d6a4b5e87dfe
Jens Langhammer <jens.langhammer@beryju.org>
2022-12-14 14:43:12 +0100

    remove tracing

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,7 @@
 import "construct-style-sheets-polyfill";
 import "./ik-gate.js";
 import * as Sentry from "@sentry/browser";
-import { BrowserTracing } from "@sentry/tracing";
 Sentry.init({
     dsn: "https://bc5df9f742f04f62bb1e4b37b227aa62@sentry.beryju.org/7",
-    integrations: [new BrowserTracing()],
-    tracesSampleRate: 1,
     tunnel: "/api/pub/sentry",
 });
